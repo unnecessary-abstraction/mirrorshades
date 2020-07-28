f76c666180e0525d05907913182de893ecfa37e5
Paul Barker <pbarker@konsulko.com>
2020-07-28 13:40:40 +0100

    Simplify main function
    Signed-off-by: Paul Barker <pbarker@konsulko.com>

--- src/mirrorshades/__init__.py
+++ src/mirrorshades/__init__.py
@@ -10,14 +10,17 @@ from . import config
 __version__ = "0.1.0-dev"
 
 
-def main():
+def parse_args():
     parser = argparse.ArgumentParser(prog="mirrorshades", description="Data mirroring tool")
     parser.add_argument("config_path", default="mirrorshades.yml", nargs="?",
                         help="path to the configuration file (defaults to 'mirrorshades.yml' "
                              "in the current directory)")
     parser.add_argument("--version", action="version", version=f"mirrorshades {__version__}")
-    args = parser.parse_args()
+    return parser.parse_args()
 
+
+def main():
+    args = parse_args()
     cfg = config.load(args.config_path)
 
     for source in cfg.sources():
