67eb9372a913384a6b391a0166ecdf1133f458d6
Paul Barker <pbarker@konsulko.com>
2020-07-28 17:15:52 +0100

    Automate release process
    Signed-off-by: Paul Barker <pbarker@konsulko.com>

--- tasks.py
+++ tasks.py
@@ -14,3 +14,16 @@ def build(c):
 def clean(c):
     """Remove build output"""
     c.run("rm -rf build dist src/*.egg-info")
+
+@task
+def release(c, version):
+    """Release a version of the project"""
+    clean(c)
+    c.run(f"sed -i 's/^\(__version__ =\).*$/\\1 \"{version}\"/' src/mirrorshades/__init__.py")
+    c.run(f"git commit -s -m 'Release {version}' src/mirrorshades/__init__.py")
+    build(c)
+    c.run(f"echo mirrorshades {version} > dist/RELEASE_NOTES.txt")
+    c.run(f"markdown-extract -fri '^{version}' ChangeLog.md >> dist/RELEASE_NOTES.txt")
+    c.run(f"git tag -a -F dist/RELEASE_NOTES.txt 'v{version}' HEAD")
+    with c.cd("dist"):
+        c.run("sha256sum * > SHA256SUMS")
