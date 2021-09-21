bddb7a40212859714300728fc6eed0c2da72fb46
Paul Barker <paul@pbarker.dev>
2021-09-21 21:10:47 +0100

    Handle failure to lookup agent by name
    Signed-off-by: Paul Barker <paul@pbarker.dev>

--- src/mirrorshades/agents/__init__.py
+++ src/mirrorshades/agents/__init__.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2020-2021 Paul Barker <paul@pbarker.dev>
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
+import sys
+
 from .command import Command
 from .git import Git
 from .github import Github
@@ -20,5 +23,9 @@ def get(properties):
     # The default connection method for a source is the name of the source
     # itself
     agent_name = properties.get("agent", properties.get("name"))
-    agent_ctor = agents[agent_name]
+    try:
+        agent_ctor = agents[agent_name]
+    except KeyError:
+        logging.error(f"No such agent: {agent_name}")
+        sys.exit(1)
     return agent_ctor(properties)
