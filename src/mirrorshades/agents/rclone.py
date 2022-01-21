666d24674e169430f53cddf7c72af4855e94a616
Paul Barker <paul@pbarker.dev>
2022-01-21 18:02:46 +0000

    agents: rclone: Print remote path before sync
    Signed-off-by: Paul Barker <paul@pbarker.dev>

--- src/mirrorshades/agents/rclone.py
+++ src/mirrorshades/agents/rclone.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2020-2022 Paul Barker <paul@pbarker.dev>
 # SPDX-License-Identifier: Apache-2.0
 
+import logging
 import os
 import subprocess
 from dataclasses import dataclass
@@ -24,6 +25,7 @@ class RClone(Agent):
                 path = ""
             full_path = ":".join([self.properties.remote, path])
             local_path = os.path.join(dest, self.properties.name, path)
+            logging.info(f"Syncing `{full_path}` via rclone")
             subprocess.run(
                 ["rclone", "sync", "--create-empty-src-dirs", full_path, local_path],
                 check=True,
