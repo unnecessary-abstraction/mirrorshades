14b046d6f8e9bc89fbf2f336a1254db423858076
Paul Barker <paul@pbarker.dev>
2023-05-08 18:00:35 +0100

    Refactor main function
    Signed-off-by: Paul Barker <paul@pbarker.dev>
    Ref: https://github.com/unnecessary-abstraction/mirrorshades/issues/26

--- src/mirrorshades/__init__.py
+++ src/mirrorshades/__init__.py
@@ -1,44 +1,4 @@
 # Copyright (c) 2020-2021 Paul Barker <paul@pbarker.dev>
 # SPDX-License-Identifier: Apache-2.0
 
-import argparse
-import logging
-import sys
-
 __version__ = "0.2.1-dev"
-
-
-def parse_args():
-    parser = argparse.ArgumentParser(
-        prog="mirrorshades", description="Data mirroring tool"
-    )
-    parser.add_argument(
-        "config_path",
-        default="mirrorshades.yml",
-        nargs="?",
-        help="path to the configuration file (defaults to 'mirrorshades.yml' "
-        "in the current directory)",
-    )
-    parser.add_argument("--source", "-s", help="select a single source to synchronize")
-    parser.add_argument(
-        "--version", action="version", version=f"mirrorshades {__version__}"
-    )
-    return parser.parse_args()
-
-
-def main():
-    from . import config
-
-    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
-    args = parse_args()
-    config.load(args.config_path)
-
-    if args.source:
-        try:
-            config.sources[args.source].mirror()
-        except KeyError:
-            logging.error(f"Source '{args.source}' does not exist in config file")
-            sys.exit(1)
-    else:
-        for source in config.sources.values():
-            source.mirror()
--- src/mirrorshades/__main__.py
+++ src/mirrorshades/__main__.py
@@ -1,6 +1,44 @@
 # Copyright (c) 2020-2021 Paul Barker <paul@pbarker.dev>
 # SPDX-License-Identifier: Apache-2.0
 
-from . import main
+import argparse
+import logging
+import sys
 
-main()
+from . import __version__, config
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        prog="mirrorshades",
+        description="An easily extensible tool for mirroring data from "
+        "git repositories, cloud storage, mail servers and other remote sources.",
+    )
+    parser.add_argument(
+        "config_path",
+        default="mirrorshades.yml",
+        nargs="?",
+        help="path to the configuration file "
+        "(defaults to 'mirrorshades.yml' in the current directory)",
+    )
+    parser.add_argument("--source", "-s", help="select a single source to synchronize")
+    parser.add_argument(
+        "--version", action="version", version=f"mirrorshades {__version__}"
+    )
+    return parser.parse_args()
+
+
+def main():
+    logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+    args = parse_args()
+    config.load(args.config_path)
+
+    if args.source:
+        try:
+            config.sources[args.source].mirror()
+        except KeyError:
+            logging.error(f"Source '{args.source}' does not exist in config file")
+            sys.exit(1)
+    else:
+        for source in config.sources.values():
+            source.mirror()
